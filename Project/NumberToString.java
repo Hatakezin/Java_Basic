e7effdb847113628c704f65f0892fe74a572fc18
Hatakezin <114512028+Hatakezin@users.noreply.github.com>
2023-10-12 19:38:22 -0300

    Add files via upload
    Um simples programa que transforma valores inteiros em valores literais. - 
    A simple program that converts integer values into literal values.

---
+++ Project/NumberToString.java
@@ -0,0 +1,23 @@
+public class NumberToString {
+    public static void main(String[] args){
+        // Example
+        metodo_01(1);
+        metodo_02(2);
+        metodo_03(3);
+    }
+
+    static void metodo_01(int number){
+        String string = String.valueOf(number);
+        System.out.println(string);
+    }
+
+    static void metodo_02(int number){
+        String string = Integer.toString(number);
+        System.out.println(string);
+    }
+
+    static void metodo_03(int number){
+        String string = "" + number;
+        System.out.println(string);
+    }
+}
